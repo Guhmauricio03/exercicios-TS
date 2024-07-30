bb6b813ca2360a8f0ac6828c8d873c787cee0d2e
Gustavo Mauricio <gustavomauricio03@gmail.com>
2024-07-30 14:36:55 -0300

    att

---
+++ Q2.ts
@@ -0,0 +1,42 @@
+// const usuarios = [
+//     {
+//         nome: "Guido",
+//         idade: 32,
+//         status: true,
+//     },
+//     {
+//         nome: "Dani",
+//         idade: 30,
+//         status: true,
+//     },
+//     {
+//         nome: "João",
+//         idade: 40,
+//         status: false,
+//     },
+//     {
+//         nome: "Guilherme",
+//         idade: 29,
+//         status: true,
+//     },
+//     {
+//         nome: "Ana",
+//         idade: 18,
+//         status: false,
+//     },
+//     {
+//         nome: "José",
+//         idade: 28,
+//         status: false,
+//     },
+// ]
+
+// const buscaUsuarios = (nomebusca: {nome: string, idade: number, status: boolean}[], nome : string):
+//     {nome: string, idade: number, status: boolean}[] => {
+//         const resultado = nomebusca.filter((usuariolista) => {
+//             return usuariolista.nome.toLowerCase().includes(nome.toLowerCase());
+//         });
+//         return resultado;
+// }
+
+// console.log(buscaUsuarios(usuarios, 'gu'));
